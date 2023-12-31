ae11fd1562c39d962ca98a599e40c92f1f800358
Aungsittkhaing <aungsittkhaing695@gmail.com>
2023-12-31 13:49:18 +0630

    Node Second Commit Branch2

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`);
 
 app.use(express.json());
+app.use(fileUpload());
+
+//to show the photo with url
+app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 
 const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
+const {saveFile, saveFiles, deleteFile} = require('./utils/gallery');
+
+//single file upload
+// app.post('/gallery',saveFile, (req, res, next) => {
+//     res.status(200).json({msg : "File Uploaded with single", "filename" : req.body.image});
+// });
+
+//multiple files uploads
+app.post('/gallery',saveFiles, (req, res, next) => {
+    res.status(200).json({msg : "File Uploaded", "filenames" : req.body.images});
+});
+
+//for single delete
+// app.post('/gallery', async(req, res, next) => {
+//     await deleteFile(req.body.name);
+//     res.status(200).json({msg:"File Deleted!"});
+// });
 
 app.use('/users', userRoute);
 app.use('/posts', postRoute);
 
+//error handling
 app.use((err, req, res, next) => {
     err.status = err.status || 200;
     res.status(err.status).json({
--- controllers/post.js
+++ controllers/post.js
@@ -1,11 +1,21 @@
+const DB = require('../databases/post');
+const Helper = require('../utils/helper');
+
 const all = async(req, res, next) => {
-    res.json({msg : 'All Post'})
+    let posts = await DB.find().populate('user', '-password -__v');
+    Helper.fMsg(res, 'All Posts', posts);
 }
 const get = async(req, res, next) => {
-    res.json({msg : 'Single post'});
+    let post = await DB.findById(req.params.id).populate('user', '-password -__v');
+    if(post){
+        Helper.fMsg(res, "Single Post", post);
+    }else{
+        next(new Error("No post with that id sir!"));
+    }
 }
 const post = async(req, res, next) => {
-    res.json({msg : 'Post new added', result : req.body});
+    let result = await new DB(req.body).save();
+    Helper.fMsg(res, "Post Added", result);
 }
 const patch = async(req, res, next) => {
     res.json({msg : 'Patch post'});
